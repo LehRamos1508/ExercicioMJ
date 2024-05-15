3a12512dd3aa6d6d75331c7d8bad25364af046d6
LehRamos1508 <2alexsander1508@gmail.com>
2024-05-14 21:47:04 -0300

    3 commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -54,20 +54,6 @@ exports.login = function (req, res) {
 
 };
 
-exports.create = function (req, res) {
-    let user = new User(
-        {
-            name: req.body.name,
-            age: req.body.age
-        }
-    );
-    user.save()
-        .then(res.status(201).send(user.toJSON()))
-        .catch((err) => {
-            res.status(500).send({ message: `${err.message} - falha ao cadastrar usuário.` })
-        })
-};
-
 exports.details = async function (req, res) {
     try {
         const result = await User.findById(req.params.id);
